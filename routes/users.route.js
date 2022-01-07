001560d4928759c010c265b39e70d5e688076c5f
amjedidiah <imunacode@gmail.com>
2022-01-07 18:18:42 +0100

    fix: Fix for users

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,39 +1,39 @@
 // ! Task 4
 function create(req, res) {
-  if (req.method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => body.push(chunk));
-
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-
-      console.log(message.split("+").join(" "));
-    });
+    if (req.method === "POST") {
+      const body = [];
+      req.on("data", (chunk) => body.push(chunk));
+  
+      req.on("end", () => {
+        const parsedBody = Buffer.concat(body).toString();
+        const message = parsedBody.split("=")[1];
+  
+        console.log(message.split("+").join(" "));
+      });
+    }
+  
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
   }
-
-  res.statusCode = 302;
-  res.setHeader("Location", "/");
-}
-
-function list(req, res) {
-  const HTML = `<html>
-  <head><title>Users</title></head>
-  <body>
-      <ul>
-        <li>Nwachukwu</li>
-        <li>Jedidiah</li>
-        <li>Jerushah</li>
-        <li>Jachi</li>
-        <li>Esther</li>
-      </ul>
-  </body>
-  <html>`;
-
-  res.write(HTML);
-}
-
-module.exports = {
-  create,
-  list,
-};
+  
+  function list(req, res) {
+    const HTML = `<html>
+    <head><title>Users</title></head>
+    <body>
+        <ul>
+          <li>User 1</li>
+          <li>User 2</li>
+          <li>User 3</li>
+          <li>User 4</li>
+          <li>User 5</li>
+        </ul>
+    </body>
+    <html>`;
+  
+    res.write(HTML);
+  }
+  
+  module.exports = {
+    create,
+    list,
+  };
